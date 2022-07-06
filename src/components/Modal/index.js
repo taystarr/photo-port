1d88cdc10de29b75bef95c0870fc5da386924ec7
Taylor Roberts <tstarrroberts@gmail.com>
2022-07-06 17:44:28 -0400

    modal testing

--- src/components/Modal/index.js
+++ src/components/Modal/index.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-function Modal({currentPhoto}) {
+function Modal({ onClose, currentPhoto }) {
     const {name, category, description, index} = currentPhoto;
 
     return (
@@ -12,7 +12,7 @@ function Modal({currentPhoto}) {
                 <p>
                     {description}
                 </p>
-                <button type="button">
+                <button onClick={onClose} type="button">
                     Close
                 </button>
             </div>
